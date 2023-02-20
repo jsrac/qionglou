3b08e9e1aab38d89a1513132e41a094341fc6285
Rhyme qing <su.n@jiangxue.team>
2023-02-20 14:39:37 +0800

    feat: fine-main

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,10 +1,22 @@
-<script setup>
-</script>
-
 <template>
-
+    <FineMain layout="center" wide="100%" high="10vw" style="border: 1px solid red">
+      <div>Main content here</div>
+    </FineMain>
 </template>
 
-<style >
+<script>
+import FineMain from "@qionglou/components/src/FineMain.vue";
 
+export default {
+  components: {
+    FineMain
+  }
+};
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+}
 </style>
