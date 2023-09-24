b09e1d438e1c146f0020c3918fc7e987ceff1be9
ᗜ ‸ ᗜ <su.n@jiangxue.team>
2023-09-25 00:01:24 +0800

    fix: `ql-divide` background color  for  transparent #17

--- packages/components/divide/src/divide.vue
+++ packages/components/divide/src/divide.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="ql-divide">
-    <ql-text style="padding-right: 15px;background-color: white"
+  <div class="ql-divide aid-back text-line-center">
+    <ql-text
              weight="medium"
              color="#9c9c9c"
+             :style="[themeStyle]"
     >
       {{ text }}
     </ql-text>
@@ -10,14 +11,51 @@
 </template>
 
 <script setup lang="ts">
+import { defineProps, computed } from "vue";
+import { createStyles } from '@emotion/vue';
+
 defineOptions({ name: 'QlDivide' })
 import { Props } from './props';
 import QlText from "../../text/src/text.vue";
 
 const props = defineProps(Props)
-const { text } = props
+const { text, theme } = props
 
+const themeStyle = computed(() => {
+  const themeStyle = {
+    default: {
+      paddingRight: '15px',
+      paddingLeft: "15px"
+    },
+    primary: {
+      fontWeight: '500',
+      color: 'rgb(255,255,255)',
+      backgroundColor: 'rgb(0, 120, 255)',
+      borderRadius: '4px !important',
+    },
+    success: {
+      fontWeight: '500',
+      color: 'rgb(255,255,255)',
+      backgroundColor: '#00dc5c',
+      borderRadius: '4px !important',
+    },
+    warning: {
+      fontWeight: '500',
+      color: 'rgb(255,255,255)',
+      backgroundColor: '#fdbf00',
+      borderRadius: '4px !important',
+    },
+    danger: {
+      fontWeight: '500',
+      color: 'rgb(255,255,255)',
+      backgroundColor: '#ff0a0a',
+      borderRadius: '4px !important',
+    }
+  };
 
+  // 如果 type 在 typeStyles 中有对应的样式，则返回对应的样式；否则返回空对象
+  return themeStyle[theme] || {};
+});
 </script>
 
 <style scoped>
--- packages/components/divide/src/props.ts
+++ packages/components/divide/src/props.ts
@@ -3,12 +3,17 @@
 import { Prop } from 'vue'
 
 export type showLayout = 'left' | 'center' | 'right'
+export type typeLayout = 'icon' | 'text'
 
 // Define the type of props.ts for Button component
 // 定义按钮组件的属性类型
 export type PropsType = {
     text: Prop<string>
-    size: Prop<showLayout>}
+    size: Prop<showLayout>
+    background: Prop<string>
+    color: Prop<string>
+    theme: Prop<string>
+}
 // Exporting the props with their respective validations and defaults.
 // 导出具有相应验证和默认值的props。
 
@@ -19,8 +24,24 @@ export const Props = {
         type: String as Prop<string>,
         default: 'test'
     },
-    type: {
+    layout: {
         type: String as Prop<showLayout>,
         default: 'test'
+    },
+    type: {
+        type: String as Prop<typeLayout>,
+        default: 'test'
+    },
+    color: {
+        type: String as Prop<string>,
+        default: 'rgb(156, 156, 156)'
+    },
+    background: {
+        type: String as Prop<string | object>,
+        default: 'white'
+    },
+    theme: {
+        type: String as Prop<string | object>,
+        default: 'default'
     }
 } as PropsType
