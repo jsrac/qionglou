893cb0ebd57c6dbb4f548d6e7ac498304713bcde
iepn <iepn@users.noreply.github.com>
2023-10-14 11:31:33 +0000

    deploy: 804bfbb297ea628a3944b90db60322560eb8a1b2

---
+++ assets/chunks/index.7156758e.js
@@ -0,0 +1,1 @@
+import{w as o}from"./install.07dc22a1.js";import t from"./table.07c276f0.js";const r=o(t);export{r as Q};
--- assets/chunks/index.78f02cb2.js
+++
@@ -1,1 +0,0 @@
-import{w as o}from"./install.07dc22a1.js";import t from"./table.bef2318f.js";const r=o(t);export{r as Q};
